2242432f605dad001a0523902a293004a36ce45d
DumitruMoraruPopovici <moraru_dumitru97@yahoo.ro>
2023-04-26 18:30:11 +0300

    Finished homework

--- src/main/java/org/example/BiathlonAthlete.java
+++ src/main/java/org/example/BiathlonAthlete.java
@@ -14,20 +14,24 @@ public class BiathlonAthlete {
     /**
      * method for adding an athlete and we return here a list of athletes
      */
-    public void addAthlete (Athlete athlete) {
+    public void addAthlete (Athlete athlete) throws Exception {
+        if(athlete == null) {
+            throw new Exception("athlete cannot be null");
+        }
+
         athleteList.add(athlete);
     }
 
     /**
      * method for sorting the list of athletes and print them in the sorted order
      */
-    public void displayAthletesByTotalTime () {
+    public List<Athlete> displayAthletesByTotalTime () {
         athleteList.sort(new FinalTimeComparator());
         int position = 1;
         for(Athlete athlete: athleteList) {
             System.out.println((position++)+ ". " + athlete);
         }
-
+        return athleteList;
     }
 
 }
